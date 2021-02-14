17f605add1c95501c3fc6b2014cca17ebccd5331
jobscale <jobscale@users.noreply.github.com>
2021-02-14 10:32:10 +0900

    process spawn updated

--- index.js
+++ index.js
@@ -2,16 +2,39 @@ require('core');
 const { spawn, exec } = require('child_process');
 
 class Shell {
+  promise() {
+    const promise = {};
+    promise.suspend = new Promise((resolve, reject) => {
+      promise.resolve = resolve;
+      promise.reject = reject;
+    });
+    return promise;
+  }
   exec(cmd) {
     const promise = promiseGen();
     exec(cmd, (e, stdout) => e ? promise.reject(e) : promise.resolve(stdout));
     return promise.instance;
   }
   spawn(command, params, options) {
-    const promise = promiseGen();
+    const { suspend, resolve } = this.promise();
+    const { res } = options || {};
+    const result = [];
     const proc = spawn(command, params, Object.assign({ shell: true }, options));
-    proc.stdout.on('data', data => promise.resolve(data.toString()));
-    return promise.instance;
+    proc.stdout.on('data', data => {
+      if (!res) result.push(data);
+      else res.write(data);
+      clearTimeout(this.timer);
+      this.timer = setInterval(() => res.write(''), 60 * 1000);
+    });
+    proc.stderr.on('data', data => {
+      logger.error(`stderr: ${data.toString()}`);
+    });
+    proc.on('close', code => {
+      logger.info(`child process exited with code ${code}`);
+      clearTimeout(this.timer);
+      resolve(result.join(''));
+    });
+    return suspend;
   }
 }
 module.exports = {
